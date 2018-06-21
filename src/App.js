b13a9a0f9992126715dfac4d55923996397f24f6
2940cristian <2940cristian@gmail.com>
2018-06-21 14:07:37 -0700

    Inintial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="body">
+
+      <header>
+        <h1>Hey, i'm Cristian.</h1>
+        <h2>And I'm a....</h2>
+      </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
