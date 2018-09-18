f39c756e75f40f1d86805f37d44f112104c797ed
2940cristian <2940cristian@gmail.com>
2018-09-18 11:19:16 -0700

    Design/Layout complete. Todo: server calls, content, and responsiveness

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Typist from "react-typist";
 import headerImg from "./Images/laptop1.jpg";
-import beachBoy from "./Images/beachBoy.jpg";
+import beachBoy from "./Images/beachBoy.jpg"
+import skilltest from "./Images/skilltest.png";
+import picme from "./Images/picme.png";
+import phone from "./Images/phone.png";
+import email from "./Images/email.png";
+import location from "./Images/location.png";
+import Axios from 'axios';
+import profile from "./Images/profile.jpg"
 
 class App extends Component {
   constructor() {
@@ -10,12 +17,20 @@ class App extends Component {
 
     this.state = {
       h2: none,
+      content: ""
     }
     this.changeStyle = this.changeStyle.bind(this)
   }
 
   componentDidMount() {
     setTimeout(this.changeStyle, 1000)
+
+    // Axios.get("https://portfolio-mail1.herokuapp.com/wakeup")
+    // .then(Response => {
+    //   console.log(Response)
+    // }).catch(err => {
+    //   alert("My server appears to have crashed, sorry about the inconvenience, bugs occur sadly")
+    // })
   }
   changeStyle() {
     this.setState({
@@ -23,18 +38,30 @@ class App extends Component {
     });
   };
 
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    
+
+  }
   
   render() {
     return (
       <div className="body">
       
       <header>
-        <img className="header__img" src={headerImg}/>
+        <img className="header__img" src={headerImg} alt="Background with tech"/>
         <div className="header__textContainer">
           <h1>Hey, I'm Cristian.</h1>
           <h2 style={this.state.h2} > <span>And I'm a....</span>
           <Typist className="blinker" avgTypingDelay={44}>
-            <Typist.Delay ms={500}/>
+            <Typist.Delay ms={1200}/>
             Web Developer
             <Typist.Backspace count={13} delay={200}/>
             Full-stack Web Developer
@@ -45,12 +72,92 @@ class App extends Component {
 
       
       <section id="about">
-        {/* <h3>About me</h3> */}
-        <div className="about__textContainer">
-          <img className="about__img" src={beachBoy} />
-          <h4 className="about__info">Officia labore exercitation proident sint aliquip sit proident. Ea mollit ullamco Lorem proident ea. Labore ea adipisicing Lorem labore laborum exercitation commodo aliqua anim aliqua eiusmod est incididunt sint. Est nisi aute laborum elit ea ad duis eu eiusmod in est aute. Consectetur consequat duis sint aliqua cillum veniam voluptate. Officia do non consequat et ipsum. Duis excepteur incididunt culpa ullamco id voluptate ipsum incididunt magna in irure Lorem.</h4>
+        <div className="about__header">
+          <h1>A little about me</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque urna, commodo et metus sit amet, ultrices dapibus nibh. Nam elementum sit amet nisi pharetra condimentum. Nullam at sollicitudin risus. Sed ac semper urna, in suscipit eros. Nulla at metus ut nibh semper bibendum sed in nulla. Sed accumsan vitae est sit amet accumsan.</p>
+        </div>
+
+        <div className="about__content">
+            <div className="about__list">
+              <ul>
+                <li className="about__list-contact">Name: <span className="about__list-span">Cristian Garcia</span> </li>
+                <li className="about__list-contact">Age: <span className="about__list-span">19 years</span> </li>
+                <li className="about__list-contact">Email: <span className="about__list-span">2940cristian@gmail.com</span> </li>
+                <li className="about__list-contact">Phone: <span className="about__list-span">760-889-6247</span> </li>
+              </ul>
+            </div>
+            <div className="about__image"><img src={profile}/></div>
+            <div className="about__list">
+            <ul>
+              <p className="about__list-contact">Skills</p>
+                <li style={{color: "rgba(0, 0, 0, 0.7)"}} className="about__list-contact">Javascript (MERN)</li>
+                <li style={{color: "rgba(0, 0, 0, 0.7)"}} className="about__list-contact">SQL</li>
+                <li style={{color: "rgba(0, 0, 0, 0.7)"}} className="about__list-contact">Python</li>
+              </ul>
+            </div>
+        </div>   
+        
+      </section>
+
+       <section id="projects">
+        <div className="projects__header">Projects:</div>
+          <div className="projects__imgs">
+            <div className="project" id="project__right">
+              <p>PicMeCollections is an image sharing platform where users can make friends and share their photos.
+                It was designed for ease of use unlike other social media.  Built by 4 Lambda School Engineers in a span of 4 weeks.
+                <br></br>
+                <br></br>
+                Technologies involved: Postgresql, Node, Express, React, Sqlite, Redux</p>
+              <img src={picme} alt="Image sharing platform"/>
+            </div>
+
+            <div className="project" id="project__left">
+              <img src={skilltest} alt="A web application for quizzing applicants"/>
+              <p><span id="project__left-WIP">(W.I.P)</span> 
+                MentrShip(not a typo), is a project being built by a team of 6 volunteers. We have the hopes of it being a 
+                completely free site where users can recieve mentorship from those who would like to offer their aid. 
+                <br></br>
+                <br></br>
+                Front-End Technologies: React, Typescript, Sass, Material-ui
+              </p></div>
+          </div>
+      </section>
+
+      <section id="contact">
+      <form id="form" method="post" action="mailto:2940cristian@gmail.com">
+          <p>Let's get in touch. Please leave your name along with either your email, phone number, or any other contact method. Will respond as quickly as possible</p>
+          <textarea onChange={this.onChange} type="text" name="content" placeholder="Ex: Hey, my name is John Doe and i'm messaging you because ...... please conact me at ...."/>
+          <button type="submit">Submit</button>
+      </form>
+
+      <section id="contact__other">
+        <div className="contact__card">
+          <img src={location}/>
+          <h3>Located AT:</h3>
+          <p>Some part in Southern California</p>
+        </div>
+
+        <div className="contact__card">
+          <img src={email}/>
+          <h3>EMAIL ME AT:</h3>
+          <p>2940cristian@gmail.com</p>
+        </div>
+
+        <div className="contact__card">
+          <img src={phone}/>
+          <h3>CALL ME AT:</h3>
+          <p>760-889-6247</p>
         </div>
       </section>
+
+      </section>
+
+
+      <footer>
+        <p>&copy; Cristian Garcia 2018</p>
+      </footer>
+
+
       </div>
     );
   }
@@ -62,7 +169,7 @@ display: "none",
 
 let show ={
   display: 'flex', 
-  display: '-webkit-box',
+  // display: '-webkit-box',
 }
 
 export default App;
